99d2ccf40f446f32da7d676456c059b41f490614
Henry de Jongh <henry00@live.nl>
2026-07-22 21:02:08 +0200

    Added root folder with distribution.

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 export default defineConfig({
   plugins: [tsconfigPaths()],
   build: {
+    outDir: 'root',
     rollupOptions: {
       output: {
         entryFileNames: 'index.js',
